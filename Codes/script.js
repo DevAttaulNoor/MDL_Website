a0ceff777001cd4f59abd754fbfcd37e84820502
Atta ul Noor <attaulnoor2001@gmail.com>
2023-07-03 18:19:18 +0500

    Modified

--- Codes/script.js
+++ Codes/script.js
@@ -11,7 +11,8 @@ if (nav_color) {
     })
 }
 
-const myCarousel = document.getElementById('myCarousel')
+// Changing the Pic of Testimonals
+let myCarousel = document.getElementById('myCarousel')
 myCarousel.addEventListener('slid.bs.carousel', function () {
     const activeItem = this.querySelector(".active");
     document.querySelector(".testi-img img").src = activeItem.getAttribute("data-img");
